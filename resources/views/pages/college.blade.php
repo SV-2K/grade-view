2579224334760288a0de30e2c1079676a8ddcb9f
SV2K <savva7446@gmail.com>
2025-03-08 14:09:33 +0700

    college and subject pages designs

--- resources/views/pages/college.blade.php
+++ resources/views/pages/college.blade.php
@@ -1,5 +1,49 @@
 @extends('layouts.main')
 
 @section('main')
-    <h1>College page</h1>
+    <div class="name-section section">
+        <h3 class="m-0">
+            Статистика колледжа
+        </h3>
+    </div>
+    <div class="container text-center mt-4">
+        <div class="row gap-4">
+            <div class="section col">
+                Column
+            </div>
+            <div class="row col-md-5 gap-4">
+                <div class="section col-md-2">
+                    Column
+                </div>
+                <div class="col gap-4 p-0">
+                    <div class="info-section">
+                        <div class="section">
+                            Средний балл:
+                        </div>
+                        <div class="section">
+                            Абсолютная успеваемость:
+                        </div>
+                        <div class="section">
+                            Качественная успеваемость:
+                        </div>
+                        <div class="section">
+                            Количество групп:
+                        </div>
+                        <div class="section">
+                            Количество студентов:
+                        </div>
+                    </div>
+                    <div class="section mt-4">
+                        Column
+                    </div>
+                </div>
+                <div class="row m-0 p-0">
+                    <div class="section ">
+                        Column
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 @endsection
--- resources/views/pages/subject.blade.php
+++ resources/views/pages/subject.blade.php
@@ -1,5 +1,45 @@
 @extends('layouts.main')
 
 @section('main')
-    <h1>Subject {{ $subject }}</h1>
+    <div class="name-section section">
+        <h3 class="m-0">
+            @php
+                echo $subject ?? 'Выберите предмет...';
+            @endphp
+        </h3>
+    </div>
+    <div class="container text-center mt-4">
+        <div class="row gap-4">
+            <div class="section col">
+                Column
+            </div>
+            <div class="col-4 gap-4 p-0">
+                <div class="info-section">
+                    <div class="section">
+                        Средний балл:
+                    </div>
+                    <div class="section">
+                        Абсолютная успеваемость:
+                    </div>
+                    <div class="section">
+                        Качественная успеваемость:
+                    </div>
+                    <div class="section">
+                        Количество групп:
+                    </div>
+                    <div class="section">
+                        Количество студентов:
+                    </div>
+                </div>
+                <div class="section mt-4" >
+                    Column
+                </div>
+            </div>
+        </div>
+        <div class="row mt-4 z-0">
+            <div class="section ">
+                Column
+            </div>
+        </div>
+    </div>
 @endsection
