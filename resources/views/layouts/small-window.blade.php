661b2765960d8bcab0d814df77aea8a897fbbf03
SV2K <savva7446@gmail.com>
2025-03-24 17:14:01 +0700

    Login and register UI

--- resources/views/layouts/small-window.blade.php
+++ resources/views/layouts/small-window.blade.php
@@ -7,25 +7,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/c3.min.css') }}">
     <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('js/d3.min.js') }}"></script>
-    <script src="{{ asset('js/c3.min.js') }}"></script>
     <title>Document</title>
 </head>
 <body>
     <main>
-        <div class="d-flex mb-4 px-4">
-            <div class="flex-grow-1">
-                @include('components.side-menu')
-            </div>
-            <div class="container mx-3 p-0" style="width: 1000px">
-                @include('components.top-section')
-                @yield('main')
-            </div>
-            <div class="flex-grow-1">
-
-            </div>
+        <div class="container section m-auto p-4" style="width: 400px">
+            @yield('main')
         </div>
     </main>
 </body>
--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.main')
+@extends('layouts.small-window')
 
 @section('main')
     @if(session()->has('error'))
@@ -6,11 +6,12 @@
             {{ session()->get('error') }}
         </div>
     @endif
+    <h2>Вход в аккаунт</h2>
     <form method="post">
         @csrf
-        <div class="mb-3">
+        <div class="mb-3 mt-4">
             <label for="email" class="form-label">E-mail</label>
-            <input type="email" class="form-control @error('email')is-invalid @enderror" value="{{ old('email') }}" name="email" id="email">
+            <input type="email" class="form-control input-bar @error('email')is-invalid @enderror" value="{{ old('email') }}" name="email" id="email">
             @error('email')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -19,14 +20,14 @@
         </div>
         <div class="mb-3">
             <label for="password" class="form-label">Пароль</label>
-            <input type="password" class="form-control @error('password')is-invalid @enderror" name="password" id="password">
+            <input type="password" class="form-control input-bar @error('password')is-invalid @enderror" name="password" id="password">
             @error('password')
                 <div class="invalid-feedback">
                     {{ $message }}
                 </div>
             @enderror
         </div>
-        <a href="{{ route('register.form') }}" class="d-block">У меня еще нет аккаунта</a>
-        <button type="submit" class="btn btn-primary mt-4">Создать аккаунт</button>
+        <a href="{{ route('register.form') }}" class="d-block link">У меня еще нет аккаунта</a>
+        <button type="submit" class="btn section mt-4">Войти в аккаунт</button>
     </form>
 @endsection
